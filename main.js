c96b8119a3469cc07994a594a9b1a9713aef3885
GabrielReds13 <92747698+GabrielReds13@users.noreply.github.com>
2021-11-03 10:55:20 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,79 @@
+/* ABRIR MENU & FECHAR */
+
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* FECHAR MENU AO CLICAR NAS OPÇÕES */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/* scroll reveal */
+const backToTopButtom = document.querySelector('.back-to-top')
+function backToTop() {
+  if (window.scrollY >= 560) {
+    backToTopButtom.classList.add('show')
+  } else {
+    backToTopButtom.classList.remove('show')
+  }
+}
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `
+#home .text, #home .image,
+#about .image, #about .text,
+#info header, #info .card,
+#testimonial header, #testimonials .testimonials,
+#contact .text, #contact .links,
+footer .brand, footer .social
+`,
+  { interval: 100 }
+)
+
+/* buttom find page */
+
+function activateMenuAtCurrentSection() {
+  const sections = document.querySelectorAll('main section[id]')
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+window.addEventListener('scroll', function () {
+  backToTop()
+  activateMenuAtCurrentSection()
+})
